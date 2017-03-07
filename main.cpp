e8513f238a23bf6825525ca0dd22eec105c811b9
begimov <begimov@gmail.com>
2017-03-07 15:28:38 +0300

    wireframe rendering

--- main.cpp
+++ main.cpp
@@ -1,75 +1,17 @@
+#include <vector>
+#include <cmath>
 #include "tgaimage.h"
+#include "model.h"
+#include "geometry.h"
 #include <iostream>
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
-
-// void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
-//     for (float t=0.; t<1.; t+=.01) {
-//         int x = x0*(1.-t) + x1*t;
-//         int y = y0*(1.-t) + y1*t;
-//         image.set(x, y, color);
-//     }
-// }
-
-// void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
-//     for (int x=x0; x<=x1; x++) {
-//         float t = (x-x0)/(float)(x1-x0);
-//         int y = y0*(1.-t) + y1*t;
-//         image.set(x, y, color);
-//     }
-// }
-
-void printInitialCoordinates(int x0, int y0, int x1, int y1) {
-  std::cout << "\n";
-  std::cout << "INITIAL COORDINATES:" << "\n";
-  std::cout << "X0: " << x0 << " " << "Y0: " << y0 << "\n";
-  std::cout << "X1: " << x1 << " " << "Y1: " << y1 << "\n";
-  std::cout << "\n";
-}
-
-void printTempCoordinates(int x0, int y0, int x1, int y1, bool steep) {
-  std::cout << "\n";
-  std::cout << "TEMPORARY COORDINATES:" << "\n";
-  std::cout << "X0: " << x0 << " " << "Y0: " << y0 << "\n";
-  std::cout << "X1: " << x1 << " " << "Y1: " << y1 << "\n";
-  std::cout << "STEEP: " << steep << "\n";
-  std::cout << "\n";
-}
-
-void printCoordinates(int x, int y) {
-  std::cout << "X: " << x << " " << "Y: " << y << "\n";
-  std::cout << "\n";
-}
-
-// void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
-//     printInitialCoordinates(x0, y0, x1, y1);
-//     bool steep = false;
-//     if (std::abs(x0-x1)<std::abs(y0-y1)) { // if the line is steep, we transpose the image
-//         std::swap(x0, y0);
-//         std::swap(x1, y1);
-//         steep = true;
-//     }
-//     if (x0>x1) { // make it left−to−right
-//         std::swap(x0, x1);
-//         std::swap(y0, y1);
-//     }
-//     printTempCoordinates(x0, y0, x1, y1, steep);
-//     for (int x=x0; x<=x1; x++) {
-//         float t = (x-x0)/(float)(x1-x0);
-//         int y = y0*(1.-t) + y1*t;
-//         if (steep) {
-//             printCoordinates(y, x);
-//             image.set(y, x, color); // if transposed, de−transpose
-//         } else {
-//             printCoordinates(x, y);
-//             image.set(x, y, color);
-//         }
-//     }
-// }
+Model *model = NULL;
+const int width  = 800;
+const int height = 800;
 
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
-    printInitialCoordinates(x0, y0, x1, y1);
     bool steep = false;
     if (std::abs(x0-x1)<std::abs(y0-y1)) {
         std::swap(x0, y0);
@@ -80,7 +22,6 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
         std::swap(x0, x1);
         std::swap(y0, y1);
     }
-    printTempCoordinates(x0, y0, x1, y1, steep);
     int dx = x1-x0;
     int dy = y1-y0;
     int derror2 = std::abs(dy)*2;
@@ -88,10 +29,8 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
     int y = y0;
     for (int x=x0; x<=x1; x++) {
         if (steep) {
-            printCoordinates(y, x);
             image.set(y, x, color);
         } else {
-            printCoordinates(x, y);
             image.set(x, y, color);
         }
         error2 += derror2;
@@ -103,12 +42,28 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
 }
 
 int main(int argc, char** argv) {
-    TGAImage image(100, 100, TGAImage::RGB);
-    // line(1, 1, 2, 2, image, white);
-    // line(13, 20, 80, 40, image, white);
-    line(20, 13, 40, 100, image, red);
-    // line(80, 40, 13, 20, image, red);
+    if (2==argc) {
+      model = new Model(argv[1]);
+    } else {
+      model = new Model("obj/african_head.obj");
+    }
+
+    TGAImage image(width, height, TGAImage::RGB);
+    for (int i=0; i<model->nfaces(); i++) {
+        std::vector<int> face = model->face(i);
+        for (int j=0; j<3; j++) {
+            Vec3f v0 = model->vert(face[j]);
+            Vec3f v1 = model->vert(face[(j+1)%3]);
+            int x0 = (v0.x+1.)*width/2.;
+            int y0 = (v0.y+1.)*height/2.;
+            int x1 = (v1.x+1.)*width/2.;
+            int y1 = (v1.y+1.)*height/2.;
+            line(x0, y0, x1, y1, image, white);
+        }
+    }
+
     image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
     image.write_tga_file("output.tga");
+    delete model;
     return 0;
 }
